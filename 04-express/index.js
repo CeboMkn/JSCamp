ec77231f6c3badebca594c34ea8678e80ebb4119
CeboMkn <195233514+CeboMkn@users.noreply.github.com>
2026-03-02 20:45:11 +0100

    Empezando con Express

---
+++ 04-express/index.js
@@ -0,0 +1,72 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use((req, res, next) => {
+  const timeString = new Date().toLocaleTimeString();
+  console.log(`[${timeString}] ${req.method} ${req.url}`);
+  next();
+})
+
+/* Ejecutar cualquier MiddleWare antes de la ruta deseada */
+
+const previusMiddleware = (req, res, next) => {
+  console.log('Ejecutando middleware previo a la ruta /');
+  next();
+}
+
+app.get('/', previusMiddleware, (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/get-jobs', (req, res) => {
+  res.json({
+    jobs: [
+      { id: 1, title: 'Desarrollador Backend', company: 'Tech Company A' },
+      { id: 2, title: 'Desarrollador Frontend', company: 'Tech Company B' },
+      { id: 3, title: 'Ingeniero de Datos', company: 'Tech Company C' },
+    ]
+  })
+})
+
+app.get('/get-single-job/:id', (req, res) => {
+  const { id } = req.params;
+  const idNumber = Number(id);
+  res.json({
+    job: { idNumber, title: `Trabajo con id ${id}` }
+  })
+})
+
+/* Rutas Opcionales */
+
+app.get('/a{b}cd', (req, res) => {
+  res.send('Ruta con letra b opcional');
+})
+
+/* Comodín */
+
+app.get('/bb*bb', (req, res) => {
+  res.send('Ruta con comodín');
+})
+
+/* Rutas mas largas que no sabes como terminan */
+
+app.get('/file/*filename', (req, res) => {
+  res.send(`Archivo solicitado: ${req.params.filename}`);
+})
+
+/* Regex, Ruta que termina con fly */
+
+app.get('.*fly$', (req, res) => {
+  res.send('Ruta que termina con fly');
+})
+
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK', message: 'Servidor saludable' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
--- 04-node/server/server.js
+++ 04-node/server/server.js
@@ -1,12 +1,99 @@
 import { createServer } from 'node:http';
+import { uptime } from 'node:process';
+import { randomUUID } from 'node:crypto';
+import { json } from 'node:stream/consumers';
 
 process.loadEnvFile()
 
+/* Puerto donde escucha nuestra aplicacion, que está en .env como variable de entorno */
 const port = process.env.PORT ?? 3000;
 
-const server = createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('Hola desde node! yass');
+/* Función para enviar respuestas JSON */
+function sendJson(res, statusCode, data) {
+    res.statusCode = statusCode;
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(JSON.stringify(data));
+}
+
+/* Usuarios */
+const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' }
+];
+
+/* Creamos el servidor */
+const server = createServer(async (req, res) => {
+
+    /* Capturamos la url y el método */
+    const { url, method } = req;
+    /* path=path query=parámetros separamos los parámetros del path*/
+    const [path, query] = url.split('?');
+    /* Creamos una constante con los params */
+    const searchParams = new URLSearchParams(query);
+
+    /* Si el método es GET */
+    if (method === 'GET') {
+        if (path === '/') {
+
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+            res.end('¡Hola, mundo!');
+            return;
+        }
+
+        if (path === '/health') {
+            sendJson(res, 200, {
+                status: 'ok',
+                uptime: Math.floor(uptime()),
+                timestamp: Date.now()
+            });
+            return;
+        }
+
+        if (path === '/users') {
+
+            /* Desde que usuario empieza */
+            const offset = Number(searchParams.get('offset')) || 0;
+            /* Cuantos me devuelv */
+            const limit = Number(searchParams.get('limit')) || users.length;
+            /* Creamos la páginacion con offset y limit (offset = 0 'muestrame desde el primero' offset + limit = 5 'hastal el resultado 5') */
+            const paginatedUsers = users.slice(offset, offset + limit);
+
+            /* Responde con un 200 y los usuarios filtrados */
+            sendJson(res, 200, paginatedUsers);
+            return;
+        }
+    }
+
+    /* Si el método es POST */
+    if (method === 'POST') {
+        if (path === '/users') {
+
+            /* Método para recuperar el body del request */
+            const body = await json(req);
+            if (!body || !body.name) {
+                return sendJson(res, 400, { error: 'Name es requerido' });
+            }
+
+            /* Creamos un objeto con el nuevo usuario */
+            const newUser = {
+                id: randomUUID(),
+                name: body.name
+            };
+
+            /* Lo pusheamos al array de usuarios */
+            users.push(newUser);
+
+            sendJson(res, 201, { message: `Usuario ${newUser.name} creado con éxito` });
+            return;
+        }
+
+    }
+
+
+
+    return sendJson(res, 404, { error: 'Not Found' });
+
 })
 
 server.listen(port, () => {
